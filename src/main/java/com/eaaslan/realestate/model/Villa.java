e2b3e07cfebef4f81a7e79ff5db6fa6540d4241c
eaaslan <ensa.aslan@gmail.com>
2025-03-26 14:26:24 +0300

    Add initial project structure with repository and model classes

---
+++ src/main/java/com/eaaslan/realestate/model/Villa.java
@@ -0,0 +1,11 @@
+package com.eaaslan.realestate.model;
+
+import java.util.UUID;
+
+public class Villa extends Residence{
+    public Villa(double price, double squareMeter, int roomCount, int salonCount) {
+        super(price, squareMeter, roomCount, salonCount);
+    }
+
+
+}
